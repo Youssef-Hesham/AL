e744bbab1a52b60f6b69f99c35e924e1b3154824
Youssef <youssef100h@gmail.com>
2022-11-03 14:32:10 +0200

    finished footer

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,4 +1,9 @@
 function Contact() {
-  return <p>contact</p>;
+  return (
+    <iframe
+      title="location"
+      src="https://maps.google.com/maps?width=709&amp;height=495&amp;hl=en&amp;q=giza%20cairo+(giza)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+    ></iframe>
+  );
 }
 export default Contact;
--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,61 @@
-import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faLinkedin,
+  faTwitter,
+  faWhatsapp,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer>
-      <p>ggg</p>
-      <p>sdsfss</p>
-      <p>dddd</p>
+      <div className="main-footer">
+        <section className="contact-info">
+          <h2>Contact Information </h2>
+          <ul>
+            <li>
+              {" "}
+              <FontAwesomeIcon icon={faPhone} /> Phone: 3586230 , 01226127068{" "}
+            </li>
+            <li>
+              {" "}
+              <FontAwesomeIcon icon={faEnvelope} /> Email:
+              alsharief353@yahoo.com
+            </li>
+            <li>
+              {" "}
+              <FontAwesomeIcon icon={faLocationDot} /> Address:
+            </li>
+            <li>
+              {" "}
+              <FontAwesomeIcon icon={faWhatsapp} /> WhatsApp: 01100538744
+            </li>
+          </ul>
+        </section>
+        <section className="footer-links">
+          <h2>Links</h2>
+          <div className="links-buttons">
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faYoutube} />
+          </div>
+        </section>
+      </div>
+
+      <div className="partners">
+        <h2>Partners</h2>
+        <div className="partner-logos">
+          <img src="images/partners/adinstruments-logo.png" alt="logo" />
+          <img src="images/partners/diagnosyslogo.png" alt="logo" />
+          <img src="images/partners/optika.png" alt="logo" />
+        </div>
+      </div>
     </footer>
   );
 }
